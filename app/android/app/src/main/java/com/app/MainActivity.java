05af448002962c6962551067b27c8dad481f3acf
hyun <coderhyun476@gmail.com>
2022-02-15 14:55:04 +0900

    Install packages

--- app/android/app/src/main/java/com/app/MainActivity.java
+++ app/android/app/src/main/java/com/app/MainActivity.java
@@ -1,13 +1,16 @@
 package com.koreanthinker.translators;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; 
+
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this); 
+        super.onCreate(savedInstanceState);
+    }
   @Override
   protected String getMainComponentName() {
     return "app";
