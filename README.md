fac08afc633a7f12519bbc9bbccee82f51b4b40f
hyun <coderhyun476@gmail.com>
2022-02-17 16:44:05 +0900

    Implement credit

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 구글, 파파고, 카카오 번역기 비교 서비스
 
 
-- cardsequence
 - credit screen
+- OSS screen
 - fab
 - translate with crawling
 - rate https://www.npmjs.com/package/react-native-in-app-review
 - icon, bundle, splash, app name
+- Android test
--- app/src/screens/CreditScreen/index.tsx
+++ app/src/screens/CreditScreen/index.tsx
@@ -1,131 +1,86 @@
-import React, {Component, useState, useRef} from 'react';
-import {
-  View,
-  Dimensions,
-  Text,
-  StatusBar,
-  Linking,
-  AsyncStorage,
-  ToastAndroid,
-} from 'react-native';
-import {BorderlessButton, BaseButton} from 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text, Linking, ToastAndroid, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import BaseButton from '../../components/BaseButton';
+import {COLORS, STATUSBAR_HEIGHT} from '../../constants/styles';
 import useNavigation from '../../hooks/useNavigation';
-
-const WIDTH = Dimensions.get('window').width;
-const RED = '#E44034';
-const ANDROIDSTORE =
-  'https://play.google.com/store/apps/details?id=com.koreanthinker.translators';
-const VERSION = '1.8.2';
+import DeviceInfo from 'react-native-device-info';
+import InAppReview from 'react-native-in-app-review';
 
 const CreditScreen = () => {
   const {goBack, navigate} = useNavigation();
 
   return (
-    <View style={{flex: 1, backgroundColor: RED}}>
-      <StatusBar backgroundColor={RED} barStyle="light-content" />
+    <View style={styles.container}>
       <BaseButton
         onPress={() => Linking.openURL('mailto:coderhyun476@gmail.com')}
-        rippleColor="#bbb"
-        style={{
-          width: '100%',
-          height: 50,
-          flexDirection: 'row',
-          alignItems: 'center',
-          marginTop: 20,
-        }}
+        style={[styles.itemContainer, {marginTop: 16}]}
       >
-        <View style={{width: 50, alignItems: 'center'}}>
+        <View style={styles.iconContainer}>
           <Icon name="email" size={20} color="white" />
         </View>
-        <Text style={{fontSize: 16, color: 'white'}}>
-          coderhyun476@gmail.com
-        </Text>
+        <Text style={styles.text}>coderhyun476@gmail.com</Text>
       </BaseButton>
       <BaseButton
         onPress={() => Linking.openURL('https://www.github.com/koreanthinker')}
-        rippleColor="#bbb"
-        style={{
-          width: '100%',
-          height: 50,
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}
+        style={styles.itemContainer}
       >
-        <View style={{width: 50, alignItems: 'center'}}>
+        <View style={styles.iconContainer}>
           <Icon name="account-circle" size={20} color="white" />
         </View>
-        <Text style={{fontSize: 16, color: 'white'}}>
-          github.com/koreanthinker
-        </Text>
+        <Text style={styles.text}>github.com/koreanthinker</Text>
       </BaseButton>
-      <BaseButton
-        onPress={() => navigate('Apis')}
-        rippleColor="#bbb"
-        style={{
-          width: '100%',
-          height: 50,
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}
-      >
-        <View style={{width: 50, alignItems: 'center'}}>
+      <BaseButton onPress={() => navigate('Oss')} style={styles.itemContainer}>
+        <View style={styles.iconContainer}>
           <Icon name="cloud" size={18} color="white" />
         </View>
-        <Text style={{fontSize: 16, color: 'white'}}>open source apis</Text>
+        <Text style={styles.text}>open source librarys</Text>
       </BaseButton>
       <BaseButton
-        onPress={() => {
-          Linking.openURL(ANDROIDSTORE);
-          AsyncStorage.setItem('RATE', 'noMore');
-        }}
-        rippleColor="#bbb"
-        style={{
-          width: '100%',
-          height: 50,
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}
+        onPress={() => InAppReview.RequestInAppReview()}
+        style={styles.itemContainer}
       >
-        <View style={{width: 50, alignItems: 'center'}}>
+        <View style={styles.iconContainer}>
           <Icon name="star" size={20} color="white" />
         </View>
-        <Text style={{fontSize: 16, color: 'white'}}>rate us</Text>
+        <Text style={styles.text}>rate us</Text>
       </BaseButton>
-      <BaseButton
-        onPress={() => {
-          ToastAndroid.show(VERSION, ToastAndroid.SHORT);
-        }}
-        rippleColor="#bbb"
-        style={{
-          width: '100%',
-          height: 50,
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}
-      >
-        <View style={{width: 50, alignItems: 'center'}}>
+      <BaseButton style={styles.itemContainer}>
+        <View style={styles.iconContainer}>
           <Icon name="info" size={20} color="white" />
         </View>
-        <Text style={{fontSize: 16, color: 'white'}}>{VERSION}</Text>
+        <Text style={styles.text}>{DeviceInfo.getVersion()}</Text>
       </BaseButton>
-      <BaseButton
-        onPress={() => goBack()}
-        rippleColor="#bbb"
-        style={{
-          width: '100%',
-          height: 50,
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}
-      >
-        <View style={{width: 50, alignItems: 'center'}}>
+      <BaseButton onPress={() => goBack()} style={styles.itemContainer}>
+        <View style={styles.iconContainer}>
           <Icon name="keyboard-backspace" size={20} color="white" />
         </View>
-        <Text style={{fontSize: 16, color: 'white'}}>뒤로</Text>
+        <Text style={styles.text}>뒤로</Text>
       </BaseButton>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.red,
+    paddingTop: STATUSBAR_HEIGHT,
+    flex: 1,
+  },
+  itemContainer: {
+    width: '100%',
+    height: 56,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconContainer: {
+    width: 56,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 16,
+    color: COLORS.white,
+  },
+});
 
 export default CreditScreen;
