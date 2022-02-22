dd8e4e8605042770c40ea9177a865d9772cdfaa8
hyun <coderhyun476@gmail.com>
2022-02-22 15:14:50 +0900

    Implement PrivacyPolicyPage test code

---
+++ web/src/pages/PrivacyPolicyPage/PrivacyPolicyPage.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import PrivacyPolicyPage from '.';
+
+describe('<PrivacyPolicyPage />', () => {
+  it('한글과 영문을 지원합니다.', async () => {
+    const { container } = render(<PrivacyPolicyPage />);
+    expect(container.innerHTML).toContain('개인정보처리방침');
+    expect(container.innerHTML).toContain('PrivacyPolicy');
+  });
+});
