6b3273cc03124a2539637cacb183abf7923b0d0d
hyun <coderhyun476@gmail.com>
2022-02-22 12:12:43 +0900

    Update privacy-policy & support page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import TranslateProvider from './context/TranslateContext';
 import HomePage from './pages/HomePage';
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
+import SupportPage from './pages/SupportPage';
 
 const App = () => {
   return (
     <TranslateProvider>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/support" element={<SupportPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+      </Routes>
     </TranslateProvider>
   );
 };
