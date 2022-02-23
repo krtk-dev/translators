343979d876bcaa678bff3fedee894ca71a3f30b0
hyun <coderhyun476@gmail.com>
2022-02-23 20:33:42 +0900

    Update privacy policy

--- web/src/pages/PrivacyPolicyPage/index.tsx
+++ web/src/pages/PrivacyPolicyPage/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 const PrivacyPolicyPage = () => {
   return (
-    <div>
+    <div style={{ padding: 20 }}>
       <h1>개인정보처리방침</h1>
-      <span>
-        앱 "3가지 번역기 비교하다"에서는 어떠한 사용자의 정보도 수집하지
-        않습니다
-      </span>
+      <div>
+        "KoreanThinker" 에서 제공하는 "3가지 번역기 비교하다"에서는 사용자분들의
+        어떠한 개인 정보도 수집하지 않습니다.
+      </div>
       <h1>PrivacyPolicy</h1>
-      <span>
-        The app "Translators" don't collects any information about users.
-      </span>
+      <div>
+        The app "Translators" provided by "KoreanThinker" doesn't collects any
+        information about users.
+      </div>
     </div>
   );
 };
